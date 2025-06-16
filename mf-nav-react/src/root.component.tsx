de660fd3c77224c100995f3489efe37f7491cd81
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-06-15 23:08:32 -0300

    nav react

---
+++ mf-nav-react/src/root.component.tsx
@@ -0,0 +1,17 @@
+export default function Root(props) {
+  return (
+    <section>
+      <nav>
+        <li>
+          <a href="/"> Home </a>
+        </li>
+        <li>
+          <a href="/@fiap/mf-angular"> MF Angular </a>
+        </li>
+        <li>
+          <a href="/@fiap/mf-react"> MF React </a>
+        </li>
+      </nav>
+    </section>
+  );
+}

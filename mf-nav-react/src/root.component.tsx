ebd3ceca980846702c2e0e5ea28a889db6a90671
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-06-16 15:32:28 -0300

    shared nav react

--- mf-nav-react/src/root.component.tsx
+++ mf-nav-react/src/root.component.tsx
@@ -1,10 +1,29 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
+import { $test, eventTest } from "@fiap/shared-data";
 
 export default function Root() {
+  const [mostrarNavbar, setMostrarNavbar] = useState(false);
+  function eventTestReact() {
+    eventTest(!mostrarNavbar);
+  }
+  useEffect(() => {
+    const subscription = $test.subscribe((value) => {
+      console.log("Valor recebido no Nav React:", value);
+      setMostrarNavbar(value);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <div className="bg-black">
-      <Navbar />
+      {!mostrarNavbar && (
+        <button className="bg-white" onClick={eventTestReact}>
+          Mostrar NavBar
+        </button>
+      )}
+      {mostrarNavbar && <Navbar />}
     </div>
   );
 }
